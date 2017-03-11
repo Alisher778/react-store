305c5de07961874c4c6f9dc5caef1865977d1d76
Alisher778 <internet-docs@mail.ru>
2017-03-10 21:39:03 -0500

    addes sequelize sync after having issue no relations found postgres

--- server.js
+++ server.js
@@ -56,5 +56,6 @@ app.get('*', (req,res)=>{
 	res.sendFile(path.join(__dirname,'public/index.html'))
 })
 
-
-app.listen(port, console.log("server started"))
+sequelize.sync().then(function(){
+  app.listen(port, console.log("server started"))
+})
