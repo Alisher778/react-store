26e49efb9b0ff2d6c17d5aafe39fd5853b314964
Alisher778 <internet-docs@mail.ru>
2017-03-10 21:24:53 -0500

    added postgress

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const Sequelize = require('sequelize');
 const databaseURL   = 'sqlite://database.sqlite3';
 const sequelize     = new Sequelize(process.env.DATABASE_URL || databaseURL);
 const port        = process.env.PORT || 3000;
+const pg = require('pg');
 //use the public folder as the static directory. 
 app.use( express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,6 +38,18 @@ app.use('/', products);
 app.use('/users', users);
 
 
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
 
 //send any route to index.html where the react app is mounted
 app.get('*', (req,res)=>{
