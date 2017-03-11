e63fc2ffcbb56bdb55341698d89657481048fc05
Alisher778 <internet-docs@mail.ru>
2017-03-10 22:19:42 -0500

    some changes

--- server.js
+++ server.js
@@ -39,17 +39,6 @@ app.use('/users', users);
 
 
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-});
 
 //send any route to index.html where the react app is mounted
 app.get('*', (req,res)=>{
